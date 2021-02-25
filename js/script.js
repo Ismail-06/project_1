f2e630d343ec4695a4fd273d7decb7b3bc8c1639
Ismail <assasin.creed.1999@bk.ru>
2021-02-25 21:38:56 +0300

    Four commit

--- js/script.js
+++ js/script.js
@@ -1,64 +1,22 @@
 'use strict';
 
-let numberOfFilms; 
-
-function start(){
-    numberOfFilms = +prompt('Сколько фильмов вы посмотрели', '');
-    while(numberOfFilms == null ||  numberOfFilms == '' ||  isNaN(numberOfFilms)){
-        numberOfFilms = +prompt('Сколько фильмов вы посмотрели', '');
+const options = {
+    name : 'test',
+    width : 1024,
+    height : 1024,
+    colors : {
+        border : 'red',
+        bg     : 'black'
     }
-}
-// start();
-
-const personalMovieDb = {
-    'count'  : numberOfFilms,
-    'movies' : {},
-    'actors' : {},
-    'genres' : [],
-    'privat' : false
 };
 
-function rememberMyFilms(){
-    for (let i = 0; i < 2; i++){
-        let a  = prompt('Последний просмотренный фильм'),
-            b  = prompt('Ваша оценка');
-            if(a != null && b != null && a != '' && b != '' && a.length < 50){
-                personalMovieDb.movies[a]  = b;
-                console.log(i);
-            } else {
-                --i;
-                console.log(i);
-            }
+for(let key in options){
+    if(typeof(options[key]) === 'object'){
+        for(let i in options[key]){
+            console.log(`Свойство ${i} имеет свойство ${options[key][i]}`);
+        }
+    } else {
+            console.log(`Свойство ${key} имеет свойство ${options[key]}`);
     }
+
 }
-
-// rememberMyFilms();
-
-function detectPersonalLevel(){
-        if(personalMovieDb.count < 10){
-        console.log('Довольно мало фильмов');
-    } else if(personalMovieDb.count > 10 && personalMovieDb.count < 30){
-        console.log('Вы классический зритель');
-    }   else if(personalMovieDb.count >= 30){
-        console.log('Вы киноман');
-    }  else {
-        console.log('произошла ошибка');
-    }
-}
-// detectPersonalLevel();
-
-
-function showMyDB(hidden){
-    if(!hidden){
-        console.log(personalMovieDb);
-    }
-}
-showMyDB(personalMovieDb.privat);
-
-
-function writeYourGenres(){
-    for(let i = 1; i <= 3; i++){
-        personalMovieDb.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
-}
-writeYourGenres();
